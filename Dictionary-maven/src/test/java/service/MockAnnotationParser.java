7c3cf5e22aa59c5d061f3f836967bda2028a2e4b
piotro <GitHubProject@gmail.com>
2012-04-01 12:30:18 +0200

    I have created annotation @Mock

--- Dictionary-maven/src/test/java/service/MockAnnotationParser.java
+++ Dictionary-maven/src/test/java/service/MockAnnotationParser.java
@@ -1,22 +1,33 @@
 package service;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 import org.easymock.EasyMock;
+import org.junit.Test;
 
 public class MockAnnotationParser {
 
 	public void parse(Class<?> clazz, Object object) throws Exception {
+		parseMethods(clazz, object);
+		parseFields(clazz, object);
+	}
 
-		Field[] fields = clazz.getDeclaredFields();
-
-		for (Field field : fields) {
+	private void parseMethods(Class<?> clazz, Object object) throws Exception {
+		for (Field field : clazz.getDeclaredFields()) {
 			if (field.isAnnotationPresent(Mock.class)) {
-				
-
 				field.setAccessible(true);
 				field.set(object, EasyMock.createMock(field.getType()));
 				System.out.println("mock: " + field.toString());
+			}
+		}
+
+	}
+
+	private void parseFields(Class<?> clazz, Object object) throws Exception {
+		for (Method method : clazz.getDeclaredMethods()) {
+			if (method.isAnnotationPresent(Test.class)) {
+				System.out.println("test method: " + method.toString());
 			}
 		}
 	}
